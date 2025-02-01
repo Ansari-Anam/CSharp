2fad0433d18baa8a682f91833340fdcd2f2db5e5
Ansari-Anam <91125199+Ansari-Anam@users.noreply.github.com>
2025-02-01 15:53:40 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace WiproTraining
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            /* First Program
+             * Console.WriteLine("Welcome");
+            Console.WriteLine("Enter the first Number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Second Number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            sum = a + b;
+            Console.WriteLine("The sum of two numbers is:" + sum
+            */
+
+
+            // Day 6 Hands On Ques 1 On arrays 
+            //SecondLargest secondLargest = new SecondLargest();
+            //int[] arr = { 12, 35, 1, 10, 34, 1 };
+            //Console.WriteLine(secondLargest.FindSecondLargest(arr));
+
+
+            // For Practice 
+            //Console.WriteLine("Enter your Name:");
+            //string Name = Console.ReadLine();
+            //Console.WriteLine("How many candies Do you want??:");
+            //int can =Convert.ToInt32(Console.ReadLine());
+            // the below code will five you 44 as output not 8 bcz the writeline will return string and the 
+            //compiler will evaluate it as string not adding twon integers 
+            //    so if you want to get 8 instead of 44 apply ()
+            //Console.WriteLine("The Nuumber Of candies You wanted is: "+ (can+4));
+        }
+
+    }
+    
+}
+
+
+ // Select the content then press ctrl+/
+// To give space to the entire code,then press alt+ctrl+take the cursor to the starting of every line
+// then release your hands from the keys and press space
+
+//  if I copy the code and with unselecting the code I press ctrl+D then it will replicate 
+//  the entire code that I have copied
+
+
